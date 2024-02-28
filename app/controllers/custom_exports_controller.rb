652b9bb4cf65e4691a5ceb7c83d6a606420415da
Mikle <semen225@gmail.com>
2024-02-29 00:09:06 +0300

    Fixes

--- app/controllers/custom_exports_controller.rb
+++ app/controllers/custom_exports_controller.rb
@@ -1,13 +1,14 @@
 class CustomExportsController < ApplicationController
 
   def index
-    @export = CustomExport.new()
-    @xmlns_rd = "urn:customs.ru:Information:CommercialFinanceDocuments:RecyclingDetails:5.22.0"
-    @xmlns_cat_ru = "urn:customs.ru:CommonAggregateTypes:5.22.0"
+    @export = CustomExport.new
   end
 
   def create
     @export = CustomExport.new(custom_export_params)
+    @xmlns_rd = "urn:customs.ru:Information:CommercialFinanceDocuments:RecyclingDetails:5.22.0"
+    @xmlns_cat_ru = "urn:customs.ru:CommonAggregateTypes:5.22.0"
+
     xml = render_to_string(layout: false)
     send_data xml, filename: "#{@export.person_surname}_#{@export.person_name}.xml", type: :xml
   end
